a2d430fa62ee18f8b20ab5100727c1bece1427e7
Lucas <lucasribeiroabreu@gmail.com>
2017-03-08 00:41:44 -0300

    Fix date

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def load_db(t):
 def get_last_modified():
     modified_at = datetime.datetime.fromtimestamp(os.path.getmtime('Banco.db'))
     new_hour = modified_at.hour - 3
-    new_hour = new_hour if new_hour > 0 else (24 - new_hour)
+    new_hour = new_hour if new_hour > 0 else (24 + new_hour)
     modified_at = modified_at.replace(hour=new_hour)
     modified_at = modified_at.strftime('%d/%m/%Y %H:%M')
 
