ea54649d548bc328609735c13f8dc7fd4effb26b
Ubuntu <ubuntu@ip-172-31-32-139.us-west-2.compute.internal>
2017-04-09 21:19:23 +0000

    synch problems fixed

--- app.py
+++ app.py
@@ -1,42 +1,62 @@
+import shutil
 from flask import Flask, redirect, render_template, url_for
 import pandas as pd
 import threading
 import datetime
 import sqlite3
 import os
+import time 
+
 pd.options.display.float_format = 'R${:,.2f}'.format
 
 
 app = Flask(__name__)
 
+def call_load_db():  
+    print("Iniciando loop")
+    c = 0
+    while True:
+        load_db(c)
+        c += 1
 
-def load_db(t):
-    if t is not None:
-        print('Esperando sincronizacao anterior')
-        t.join()
-        return
-    # Load
-    print('Loading data')
-    conn = sqlite3.connect('../OneDrive/Documentos/Banco.db')
-
+def load_db(c):
+   
+    print("Loading db..")
+    os.system("onedrive-d restart")
+    print("Onedrive restartado")
+    os.remove('Banco.db')
+    print("Copiando arquivo do Banco")
+    shutil.copyfile('../OneDrive/Documentos/Banco.db', 'Banco.db')
+    print("Banco copiado")
+    print('Conectando ao banco local copiado')
+    conn = sqlite3.connect('Banco.db')
 
     df_contas = pd.read_sql_query("SELECT * from CONTAS", conn)
     df_contas.DATA = pd.to_datetime(df_contas.DATA, format='%d/%m/%Y %H:%M:%S')
     df_prods = pd.read_sql_query("SELECT * from PRODUTOS", conn, parse_dates=False)
-
+    print("Tudo certo nas queries")
     # Clean data
     df_contas.DATA = pd.to_datetime(df_contas.DATA)
     df_contas = df_contas[df_contas.NOME != 'Excluido']
     df_contas.index = pd.to_datetime(df_contas.DATA, dayfirst=False, yearfirst=True)
-    print('Cleaning data')
+    # print('Cleaning data')
     df_contas = df_contas[df_contas.DATA >= '2016-01-01']
     df_contas.to_csv('data.csv', quoting=2, index=False)
-    print('OK')
-    return
-
+  
+    with open("static/log.txt", "a") as f:
+        f.write(", ".join([str(c), "Tamanho:", str(len(df_contas)), str(datetime.datetime.now())]))
+        f.write("\n")
+    print("Sleeping")
+    time.sleep(60)
+    print("Pronto")
+    return True
 
 def get_last_modified():
-    modified_at = datetime.datetime.fromtimestamp(os.path.getmtime('Banco.db')).strftime('%d/%m/%Y %H:%M')
+    try:
+        modified_at = datetime.datetime.fromtimestamp(os.path.getmtime('Banco.db')).strftime('%d/%m/%Y %H:%M')
+    except:
+        pass
+    # print('Last modified: {}'.format(modified_at))
 
     return modified_at
 
@@ -47,7 +67,8 @@ def get_report(df_contas):
     df_contas.Data = pd.to_datetime(df_contas.Data)
     df_contas.set_index('Data', inplace=True)
 
-    report = df_contas.tail(50).sort_index(ascending=False).to_html(classes=['table', 'table-hover'])
+    report = df_contas.tail(50).sort_index(ascending=False)[['Conta', 'Código Produto', 'Produto', 'Preço']].to_html(classes=['table', 'table-hover', 'highchart'])
+   
     reports['last_n'] = report
 
 
@@ -56,7 +77,7 @@ def get_report(df_contas):
         rp.index = rp.index.date
         rp.columns = ['Total']
         reports[i] = rp.T\
-            .to_html(classes=['table', 'table-hover'])
+            .to_html(classes=['table', 'table-hover', 'highchart'])
         # reports[i] = rp.T.style.background_gradient('winter', axis=1).render()
 
     return reports
@@ -64,11 +85,6 @@ def get_report(df_contas):
 
 @app.route("/")
 def hello():
-    global t
-    t = threading.Thread(target=load_db, args=(t,))
-    t.daemon = True
-    t.start()
-
     data = dict()
 
     if os.path.exists('data.csv'):
@@ -81,6 +97,15 @@ def hello():
         data['msg'] = 'Carregando..Tente novamente em alguns segundos'
         return render_template('main.html', data=data)
 
-t = None
+@app.route('/log')
+def root():
+    if os.path.exists("static/log.txt"):
+        return app.send_static_file('log.txt')
+    else:
+        return "No logs"
+t = threading.Thread(target=call_load_db)
+t.daemon = True
+t.start()
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
